decc3f2a99c9ae8bfecf2815b0a29fc1a37bb4df
DriesDelanghe'git config --global user.email delanghe.dries.1998@gmail.comgoit commit -m aanpassing <delanghe.dries.1998@gmail.com>
2020-12-15 10:13:45 +0100

    removed files I used as examples

--- VoorbeeldProf/week2/src/oefeningenbundel2/TestDatum.java
+++
@@ -1,9 +0,0 @@
-package oefeningenbundel2;
-
-public class TestDatum {
-    public static void main(String[] args) {
-        Datum d1 = new Datum(40,50,2000);
-        Datum d2 = new Datum();
-        System.out.println(d1);
-    }
-}
